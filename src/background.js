a60fd3e0aa7eb9a726315d560b9f8977faf5272b
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-09-25 00:47:06 +0700

    Update background

--- src/background.js
+++ src/background.js
@@ -1,18 +1,32 @@
-chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
+var pki = require('../forge/pki.js');
+var axios = require('axios');
+var downloadjs = require('downloadjs');
+
+var generateAndSendCSR = function(tokenAccess, tokenCSR, passphrase) {
+  console.log('generateAndSendCSR');
+  console.log(tokenAccess);
+  console.log(tokenCSR);
+  console.log(passphrase);
+}
+
+var downloadCertAndPackP12 = function(tokenAccess, tokenCSR, passphrase) {
+  console.log('downloadCertAndPackP12');
+  console.log(tokenAccess);
+  console.log(tokenCSR);
+  console.log(passphrase);
+}
+
+chrome.runtime.onMessageExternal.addListener(function(msg, sender, sendResponse) {
   if (sender.url !== "http://localhost:3000/") {
     console.log('Not a valid host');
     alert('Not a valid host : ' + sender.url);
     sendResponse(false);
     return;
   }
-  // Remove existing key value
-  chrome.storage.sync.remove('current-csr', function(){
-    var obj = {}
-    obj['current-csr'] = request.record;
-    chrome.storage.sync.set(obj, function(){
-      console.log('csr-request saved');
-      alert('CSR attribute data has been saved, please click BSSN PKI Playground extension icon to continue.');
-      sendResponse(true);
-    });
-  });
+
+  if (msg === 'generateAndSendCSR') {
+    generateAndSendCSR(msg.tokenAccess, msg.tokenCSR, msg.passphrase);
+  } else if (msg === 'fetchCertAndPackP12') {
+    downloadCertAndPackP12(msg.tokenAccess, msg.tokenCSR, msg.passphrase);
+  }
 });
